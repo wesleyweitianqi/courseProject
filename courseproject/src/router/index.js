0b84f6b6f21d69088d180e677003c904a793030d
wesleyweitianqi <wesleytianqiwei@gmail.com>
2023-02-07 21:56:33 -0500

    router interception and setup

--- courseproject/src/router/index.js
+++ courseproject/src/router/index.js
@@ -1,13 +1,26 @@
+import { ElMessage } from "element-plus";
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     { path: "/", redirect: "/login" },
-    { path: "/home", component: () => import("../views/Home") },
+    { path: "/home", component: () => import("../views/Home"), meta: {isAuth: true} },
     { path: "/login", component: () => import("../views/Login") },
     { path: "/register", component: () => import("../views/Register") },
   ],
 });
+
+router.beforeEach((to, from, next)=> {
+  if (to.meta.isAuth) {
+    if (localStorage.getItem("token")) {
+      next()
+    } else {
+      ElMessage.error('please login')
+    }
+  } else {
+    next()
+  }
+})
 
 export default router;
--- courseproject/src/views/Home.vue
+++ courseproject/src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <div>Home</div>
-    <el-button type="primary" @click="toGo('/login')">Login</el-button>
   </div>
+  <el-button type="primary" @click="toGo('login')">Login</el-button>
 </template>
 <script setup>
-  import router from '../router/index' ;
-  const toGO = (val)=> {
-    router.push(val);
-  }
+import router from '../router/index' ;
+const toGo=(val)=> {
+  router.push(val)
+}
 </script>
 <style lang='less' scoped>
 </style>
